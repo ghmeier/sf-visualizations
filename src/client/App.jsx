9443bb4cc98470673274e9b873a34e37b9bc3716
Garret Meier <garret@mixmax.com>
2020-04-08 00:41:20 -0700

    feat: add support for case graphs

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,10 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line, Text } from 'recharts';
 
 const icuColor = '#0000cc';
 const acuteColor = '#00cc66';
 
-function toRelativeData(list) {
+function toHospitaleData(list) {
   return list.map((item) => ({
     name: new Date(item.reported).toLocaleDateString(),
     icuChange: item.ICU.percentChange,
@@ -15,11 +16,35 @@ function toRelativeData(list) {
   }));
 }
 
+function toCaseData(list) {
+  return list.map((item) => ({
+    name: new Date(item.reported).toLocaleDateString(),
+    communityChange: item.Community.percentChange,
+    deathsChange: item.Deaths.percentChange,
+    unknownChange: item.Unknown.percentChange,
+    fromContactChange: item['From Contact'].percentChange,
+    communityIncrease: item.Community.increase,
+    deathsIncrease: item.Deaths.increase,
+    unknownIncrease: item.Unknown.increase,
+    fromContactIncrease: item['From Contact'].increase,
+    communityCumulative: item.Community.cumulative,
+    deathsCumulative: item.Deaths.cumulative,
+    unknownCumulative: item.Unknown.cumulative,
+    fromContactCumulative: item['From Contact'].cumulative,
+    total: item.total,
+    change: item.change,
+  }));
+}
+
 function VerticalLabel({ text }) {
   return (
-    <Text x={-10} y={0} dx={50} dy={150} offset={0} angle={-90}>{text}</Text>
+    <Text x={-10} y={0} dx={50} dy={150} offset={0} angle={-90}>
+      {text}
+    </Text>
   );
 }
+
+VerticalLabel.propTypes = { text: PropTypes.string.isRequired };
 
 export default class App extends React.Component {
   constructor(props) {
@@ -31,22 +56,36 @@ export default class App extends React.Component {
   componentDidMount() {
     fetch('/api/hospitalizations')
       .then((response) => response.json())
-      .then((data) => {
-        this.setState({
-          relative: toRelativeData(data),
-          absolute: toRelativeData(data),
-        });
-      });
+      .then((data) => this.setState({ hospitalizations: toHospitaleData(data) }));
+    fetch('/api/cases')
+      .then((response) => response.json())
+      .then((data) => this.setState({ cases: toCaseData(data) }));
   }
 
   render() {
     return (
       <div className='flexbox'>
+        <h2 className='pl5-l'>Total San Francisco Hospital bed use over time</h2>
+        <LineChart
+          width={730}
+          height={300}
+          data={this.state.hospitalizations}
+          margin={{ top: 5, right: 30, left: 20, bottom: 50 }}>
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='name' />
+          <YAxis label={<VerticalLabel text='Beds in Use' />} />
+          <Tooltip />
+          <Legend />
+          <Line type='monotone' name='ICU' dataKey='icuTotal' stroke={icuColor} />
+          <Line type='monotone' name='Acute Care' dataKey='acuteTotal' stroke={acuteColor} />
+          <Line type='monotone' name='Total' dataKey='total' stroke='#003366' />
+        </LineChart>
+
         <h2 className='pl5-l'>% Change in San Francisco Hospital bed use over time</h2>
         <LineChart
           width={730}
           height={300}
-          data={this.state.relative}
+          data={this.state.hospitalizations}
           margin={{ top: 5, right: 30, left: 20, bottom: 50 }}>
           <CartesianGrid strokeDasharray='3 3' />
           <XAxis dataKey='name' />
@@ -57,20 +96,66 @@ export default class App extends React.Component {
           <Line type='monotone' name='Acute Care' dataKey='acuteChange' stroke={acuteColor} />
         </LineChart>
 
-        <h2 className='pl5-l'>Total San Francisco Hospital bed use over time</h2>
+        <h2 className='pl5-l'>Total San Francisco Cases over time by Transmission Type</h2>
         <LineChart
           width={730}
           height={300}
-          data={this.state.absolute}
+          data={this.state.cases}
           margin={{ top: 5, right: 30, left: 20, bottom: 50 }}>
           <CartesianGrid strokeDasharray='3 3' />
           <XAxis dataKey='name' />
-          <YAxis domain={[0, 100]} label={<VerticalLabel text='Beds in Use' />} />
+          <YAxis label={<VerticalLabel text='Number of Cases' />} />
           <Tooltip />
           <Legend />
-          <Line type='monotone' name='ICU' dataKey='icuTotal' stroke={icuColor} />
-          <Line type='monotone' name='Acute Care' dataKey='acuteTotal' stroke={acuteColor} />
           <Line type='monotone' name='Total' dataKey='total' stroke='#003366' />
+          <Line type='monotone' name='Community' dataKey='communityCumulative' stroke={icuColor} />
+          <Line type='monotone' name='Unknown' dataKey='unknownCumulative' stroke={acuteColor} />
+          <Line type='monotone' name='Direct Contact' dataKey='fromContactCumulative' stroke='#003366' />
+        </LineChart>
+
+        <h2 className='pl5-l'>Change in San Francisco Cases Over Time</h2>
+        <LineChart
+          width={730}
+          height={300}
+          data={this.state.cases}
+          margin={{ top: 5, right: 30, left: 20, bottom: 50 }}>
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='name' />
+          <YAxis domain={[0, 200]} label={<VerticalLabel text='% Change in Cases' />} />
+          <Tooltip />
+          <Legend />
+          <Line type='monotone' name='Total' dataKey='change' stroke='#003366' />
+          <Line type='monotone' name='Community' dataKey='communityChange' stroke={icuColor} />
+          <Line type='monotone' name='Unknown' dataKey='unknownChange' stroke={acuteColor} />
+          <Line type='monotone' name='Direct Contact' dataKey='fromContactChange' stroke='#003366' />
+        </LineChart>
+
+        <h2 className='pl5-l'>Total San Francisco Deaths Over Time</h2>
+        <LineChart
+          width={730}
+          height={300}
+          data={this.state.cases}
+          margin={{ top: 5, right: 30, left: 20, bottom: 50 }}>
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='name' />
+          <YAxis label={<VerticalLabel text='Number of Deaths' />} />
+          <Tooltip />
+          <Legend />
+          <Line type='monotone' name='Total' dataKey='deathsCumulative' stroke='#003366' />
+        </LineChart>
+
+        <h2 className='pl5-l'>Change in San Francisco Deaths Over Time</h2>
+        <LineChart
+          width={730}
+          height={300}
+          data={this.state.cases}
+          margin={{ top: 5, right: 30, left: 20, bottom: 50 }}>
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='name' />
+          <YAxis domain={[0, 200]} label={<VerticalLabel text='% Change in Deaths' />} />
+          <Tooltip />
+          <Legend />
+          <Line type='monotone' name='Total' dataKey='deathsChange' stroke='#003366' />
         </LineChart>
       </div>
     );
--- src/server/api.js
+++ src/server/api.js
@@ -6,12 +6,17 @@ const BASE_URL = 'https://data.sfgov.org/resource/';
 
 const RESOURCE_MAP = {
   hospitalizations: 'nxjg-bhem.json',
+  cases: 'tvq9-ec9w.json',
 };
 
-const categories = ['ICU', 'Med/Surg'];
+const hospitalCategories = ['ICU', 'Med/Surg'];
 
 function resourceUrl(resource) {
   return `${BASE_URL}${RESOURCE_MAP[resource]}`;
+}
+
+function percentChange(previous, current) {
+  return previous === 0 ? 0 : ((current / previous) * 100 - 100).toFixed(2);
 }
 
 router.get('/hospitalizations', async (req, res) => {
@@ -26,24 +31,55 @@ router.get('/hospitalizations', async (req, res) => {
   });
 
   const results = _.sortBy(
-    _.map(byDate, (value, key) => ({ ...value, reported: key })),
-    ({ reported }) => new Date(reported)
+    _.map(byDate, (value) => value),
+    ({ reported }) => reported
   );
 
-  const previous = { ICU: 0, 'Med/Surg': 0, total: 0 };
+  const previous = { ICU: 0, 'Med/Surg': 0 };
   results.forEach((value) => {
-    categories.map((key) => {
-      const currentPatientTotal = value[key].total;
-      const previousPatientTotal = previous[key];
-      const patienCountChange = currentPatientTotal - previousPatientTotal;
-      const percentChange =
-        previousPatientTotal === 0
-          ? 0
-          : Math.floor((patienCountChange / previousPatientTotal) * 100);
-
-      value[key].percentChange = percentChange;
+    hospitalCategories.map((key) => {
+      value[key].percentChange = percentChange(previous[key], value[key].total);
       previous[key] = value[key].total;
     });
+  });
+
+  res.json(results);
+});
+
+const transmissionCategories = ['Community', 'From Contact', 'Unknown'];
+const caseCategories = [...transmissionCategories, 'Deaths'];
+
+router.get('/cases', async (req, res) => {
+  const allCases = await got(resourceUrl('cases')).json();
+  const byDate = {};
+  allCases.forEach(({ date, case_count, transmission_category, case_disposition }) => {
+    date = new Date(date).toLocaleDateString();
+    const key = case_disposition === 'Deaths' ? case_disposition : transmission_category;
+    if (!byDate[date]) {
+      byDate[date] = {};
+      caseCategories.forEach((category) => {
+        byDate[date][category] = { increase: 0, cumulative: 0, percentChange: 0 };
+      });
+    }
+    byDate[date][key].increase += parseInt(case_count, 10);
+    byDate[date].reported = new Date(date);
+  });
+
+  const results = _.sortBy(Object.values(byDate), ({ reported }) => new Date(reported));
+
+  const previous = { Community: 0, 'From Contact': 0, Unknown: 0, Deaths: 0, total: 0 };
+  results.forEach((value) => {
+    caseCategories.map((key) => {
+      const { increase } = value[key];
+      value[key].cumulative = previous[key] + increase;
+      value[key].percentChange = percentChange(previous[key], value[key].cumulative);
+
+      previous[key] = value[key].cumulative;
+    });
+
+    value.total = _.sumBy(transmissionCategories, (category) => value[category].cumulative);
+    if (previous.total !== 0) value.percentChange = percentChange(previous.total, value.total);
+    previous.total = value.total;
   });
 
   res.json(results);
